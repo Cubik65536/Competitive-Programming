a0df728c8225447e6a81ad14c05512ad25dde100
Cubik <cubik65536@cubik65536.top>
2023-02-16 23:23:26 -0500

    feat: Add C++ Template Code File

---
+++ CPP_Template_Simple.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // freopen("file.in", "r", stdin);
+    // freopen("file.out", "w", stdout);
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    return 0;
+}
